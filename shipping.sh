9b42ac984291247061bb89d8b8683c8df1ee608f
Keshav-Code-er <keshavinsc446@gmail.com>
2025-05-15 17:12:08 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -74,7 +74,7 @@ yum install mysql -y  &>>$LOGFILE
 
 VALIDATE $? "Install mysql"
 
-mysql -h  mysql.joindevops.shop -uroot  -RoboShop@1 < /app/schema/shipping.sql  &>>$LOGFILE
+mysql -h  mysql.joindevops.shop -uroot  -RoboShop@1 < /app/db/schema.sql  &>>$LOGFILE
 
 VALIDATE $? "Load Schema"
 
